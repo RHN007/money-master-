364e5867e107434eed845a87fef78e9896c04379
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2022-02-17 18:54:29 +0600

    finalizing calculation

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
-// Handle Calculate Button :
+//Income Function
 
+function getIncomeValue() {
+    const incomeInput = document.getElementById("income-input");
+    const incomeAmount = parseFloat(incomeInput.value);
+    return incomeAmount;
+}
+
+//Expense Function
+function getInputValue(expense) {
+    const expenseInput = document.getElementById(expense + "-expense-input");
+    const expenseAmount = parseFloat(expenseInput.value);
+    return expenseAmount;
+}
+
+function balance() {
+    const foodExpnese = getInputValue("food");
+    const rentExpense = getInputValue("rent");
+    const clothExpense = getInputValue("cloth");
+    const expenseTotal = parseFloat(foodExpnese + rentExpense + clothExpense);
+    const balanceTotal = getIncomeValue() - expenseTotal;
+
+    document.getElementById("expense-total").innerText = expenseTotal;
+    document.getElementById("balance-total").innerText = balanceTotal;
+
+    return balanceTotal;
+}
+
+// Handle Calculate Balance Event :
 document
     .getElementById("calculate-button")
     .addEventListener("click", function() {
-        //Income Input
-
-        const incomeInput = document.getElementById("income-input");
-        const incomeAmount = parseFloat(incomeInput.value);
-
-        //Expense Function
-        function getInputValue(expense) {
-            const expenseInput = document.getElementById(expense + "-expense-input");
-            const expenseAmount = parseInt(expenseInput.value);
-            return expenseAmount;
-        }
-
-        function calculateTotalExp() {
-            const foodExpnese = getInputValue("food");
-            const rentExpense = getInputValue("rent");
-            const clothExpense = getInputValue("cloth");
-            const expenseTotal = parseFloat(foodExpnese + rentExpense + clothExpense);
-            document.getElementById("expense-total").innerText = expenseTotal;
-            return expenseTotal;
-        }
-
-        // Get Current Balance
-        const balanceTotal = document.getElementById("balance-total");
-        balanceTotal.innerText = incomeAmount - calculateTotalExp();
+        balance();
     });
+
+// Handle Savings Event
+
+document.getElementById("saving-button").addEventListener("click", function() {
+    const savingsInput = document.getElementById("saving-input");
+    const savingsInputPercent = parseFloat(savingsInput.value);
+
+    const savings = (getIncomeValue() / 100) * savingsInputPercent;
+    const remainingBalance = parseFloat(balance() - savings);
+
+    document.getElementById("savings-amount").innerText = savings;
+    document.getElementById("remaining-balance").innerText = remainingBalance;
+});
