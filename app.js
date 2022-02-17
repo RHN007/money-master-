bfed6b929cb36917c0c5cabbcdf8ffeee1dcdb01
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2022-02-17 17:04:50 +0600

    finishing income and expense

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log(5);
+// Handle Income Input :
+
+document
+    .getElementById("calculate-button")
+    .addEventListener("click", function() {
+        //Income Input
+
+        const incomeInput = document.getElementById("income-input");
+        const incomeAmount = parseFloat(incomeInput.value);
+
+        //Expense Function
+        function getInputValue(expense) {
+            const expenseInput = document.getElementById(expense + "-expense-input");
+            const expenseAmount = parseInt(expenseInput.value);
+            return expenseAmount;
+        }
+
+        function calculateTotalExp() {
+            const foodExpnese = getInputValue("food");
+            const rentExpense = getInputValue("rent");
+            const clothExpense = getInputValue("cloth");
+
+            const expenseTotal = parseFloat(foodExpnese + rentExpense + clothExpense);
+
+            document.getElementById("expense-total").innerText = expenseTotal;
+
+            return expenseTotal;
+        }
+
+        // Get Current Balance
+        const balanceTotal = document.getElementById("balance-total");
+        const previousBalanceText = balanceTotal.innerText;
+        const previousBalanceAmount = parseFloat(previousBalanceText);
+        const newBalanceTotal =
+            incomeAmount + previousBalanceAmount - calculateTotalExp();
+        balanceTotal.innerText = newBalanceTotal;
+    });
